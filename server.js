b9221687d54dc8e8eafec2d805a87d456513e7c6
yashchauhanaccenture <y.b.singh.chauhan@accenture.com>
2022-12-02 19:01:25 +0530

    third

--- server.js
+++ server.js
@@ -2,42 +2,48 @@ const mongodb = require("mongodb").MongoClient;
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.get("/api", (req, res) => {
+  try {
     mongodb.connect("mongodb://127.0.0.1:27017", (err, conn) => {
-        if (err) throw err;
-        const db = conn.db("demotf");
-        const coll = db.collection("Student");
-        coll.find({}).toArray((err, docs) => {
-            if (err) throw err;
-            res.send(docs);
-            console.log(docs)
-        });
+      if (err) throw err;
+      const db = conn.db("demotf");
+      const coll = db.collection("Student");
+      coll
+        .find()
+        .toArray()
+        .then((docs) => res.send(docs))
+        .catch((err) => errorCatcher(err));
     });
-
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/", (req, res) => {
+app.post("/add", (req, res) => {
+  try {
     mongodb.connect("mongodb://127.0.0.1:27017", (err, conn) => {
-        if (err) throw err;
-        const db = conn.db("demotf");
-        const coll = db.collection("Student");
-        coll.insertOne({}).toArray((err, docs) => {
-            if (err) throw err;
-            res.send(docs);
-            console.log(docs)
-        });
+      if (err) throw err;
+      const db = conn.db("demotf");
+      const coll = db.collection("Student");
+      const newData = req.body;
+      coll.insertOne(newData);
     });
-
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-
-app.listen(3001, () => console.log("Server listening"));
-
+app.listen(4000, () => console.log("Server listening"));
