37aa82aaac6de1815de9d07d46810281b1f05673
yashchauhanaccenture <y.b.singh.chauhan@accenture.com>
2022-12-05 19:26:26 +0530

    editpart

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
+const ObjectID = require('mongodb').ObjectID;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,7 +26,7 @@ app.get("/api", (req, res) => {
         .find()
         .toArray()
         .then((docs) => res.send(docs))
-      
+
     });
   } catch (err) {
     console.log(err);
@@ -46,4 +47,22 @@ app.post("/add", (req, res) => {
   }
 });
 
-app.listen(4000, () => console.log("Server listening"));
+
+app.put("/:id", (req, res, next) => {
+  mongodb.connect("mongodb://127.0.0.1:27017", (err, conn) => {
+    if (err) throw err;
+    const db = conn.db("demotf");
+    const coll = db.collection("Student");
+    coll.findOneAndUpdate({_id: ObjectID(req.params.id)}, {$set: req.body}, (error, data) => {
+      if (error) {
+        console.log(error)
+      }
+      else {
+        res.json(data)
+        console.log('Student updated successfully !')
+      }
+    })
+  })
+})
+
+  app.listen(4000, () => console.log("Server listening"));
